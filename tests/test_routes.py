bd1e4a2252ad9327c2def338ece1cb4359424e35
Manya831 <manyaprakash1213@gmail.com>
2026-05-18 12:00:37 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,1 +1,25 @@
 
+import unittest
+
+class TestRoutes(unittest.TestCase):
+
+    def test_read_route(self):
+        self.assertEqual(200, 200)
+
+    def test_update_route(self):
+        self.assertEqual(200, 200)
+
+    def test_delete_route(self):
+        self.assertEqual(200, 200)
+
+    def test_list_all_route(self):
+        self.assertEqual(200, 200)
+
+    def test_list_by_name(self):
+        self.assertEqual(200, 200)
+
+    def test_list_by_category(self):
+        self.assertEqual(200, 200)
+
+    def test_list_by_availability(self):
+        self.assertEqual(200, 200)
