75faf56385e8f8e0cdfcde23f95069b404aa1a77
Manya831 <manyaprakash1213@gmail.com>
2026-05-18 12:10:21 +0530

    Update web_steps.py

--- features/features/features/steps/web_steps.py
+++ features/features/features/steps/web_steps.py
@@ -1,1 +1,14 @@
 
+from behave import then
+
+@then('I should see text')
+def step_impl(context):
+    assert True
+
+@then('text should not be present')
+def step_impl(context):
+    assert True
+
+@then('message is displayed')
+def step_impl(context):
+    assert True
